2d36caeb2e42e712b81482dd94fbae78697911b2
Judyta <jdtwlodar@gmail.com>
2016-06-12 21:53:49 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ $(function(){
 $(function(){
  var windowWidth=$(window).width();
 	if (windowWidth > 1200) {
+
         $(window).scroll(function() {
             $('.animatedEntrance').each(function() {
                 var imagePos = $(this).offset().top;
@@ -86,5 +87,58 @@ $(function(){
     });
     });
 
-/*
-$('.carousel').carousel();*/
+
+
+/*validacja formularza*/
+
+document.addEventListener('DOMContentLoaded', function(){
+    var form = document.querySelector('form');
+    console.log(form);
+
+    form.addEventListener('submit', function(){
+        console.log(this.elements);
+        var isValid=true;
+        for(var i = 0; i<this.elements.length; i++){
+            //this.elements[i];
+
+
+            if(this.elements[i].id==='exampleInputName'){
+                // console.log(this.elements[i].id==='name')
+                if(this.elements[i].value.length<=5){
+                    this.elements[i].style.borderColor = 'red';
+                    isValid=false;
+                }
+                else{
+                    this.elements[i].style.borderColor = '#9f449b';
+                    isValid=true;
+                }
+            };
+            if(this.elements[i].id==='exampleInputEmail1'){
+                if(this.elements[i].value.indexOf('@')===-1) {
+                    this.elements[i].style.borderColor = 'red';
+                    isValid = false;
+                }
+                else{
+                    this.elements[i].style.borderColor = '#9f449b';
+                    isValid=true;
+                }
+            };
+            if(this.elements[i].id==='exampleInputMessage'){
+                if(this.elements[i].value.length<=5){
+                    this.elements[i].style.borderColor = 'red';
+                    isValid=false;
+                }
+                else{
+                    this.elements[i].style.borderColor = '#9f449b';
+                    isValid=true;
+                }
+            };
+
+
+            if(!isValid){
+                event.preventDefault()}
+        }
+
+    });
+
+});
