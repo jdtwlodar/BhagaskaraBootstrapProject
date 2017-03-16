a4e39018be6f2dd35ea2930da20d05775c01feb1
judyta wlodarska <jdtwlodar@gmail.com>
2017-03-16 20:52:43 +0100

    counter added

--- app.js
+++ app.js
@@ -63,3 +63,28 @@ $(function() {
 
 
 });
+
+
+//counter
+$(window).scroll(function() {
+    var hT = $('.counterwrapper').offset().top,
+        hH = $('.counterwrapper').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    console.log((hT - wH), wS);
+    if (wS > (hT + hH - wH)) {
+        $('.count').each(function() {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function(now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        }); {
+            $('.count').removeClass('count').addClass('counted');
+        };
+    }
+});
